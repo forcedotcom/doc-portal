d13019b7b86d1e41031bbeda67d467523da2e3e6
Steve Anderson <sanderson@salesforce.com>
2013-11-13 17:11:08 -0800

    Adding license info

--- application.rb
+++ application.rb
@@ -1,3 +1,31 @@
+#Copyright (c) 2013,salesforce.com 
+#All rights reserved.
+
+#Redistribution and use in source and binary forms, with or without
+#modification, are permitted provided that the following conditions
+#are met:
+
+#Redistributions of source code must retain the above copyright
+#notice, this list of conditions and the following disclaimer.
+
+#Redistributions in binary form must reproduce the above copyright
+#notice, this list of conditions and the following disclaimer in the
+#documentation and/or other materials provided with the distribution.
+
+#THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+#"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+#LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+#A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+#HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+#INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+#BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+#OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+#AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+#LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY
+#WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+#POSSIBILITY OF SUCH DAMAGE.
+
+
 require 'bundler'
 Bundler.require(:default, (ENV['RACK_ENV'] || :development).to_sym)
 require_relative 'config/setup.rb'
@@ -358,6 +386,7 @@ module SFDC
     def redirect_to_default_topic(locale, version, deliverable = nil)
       locale = set_locale(locale)
       version = set_version(version)
+#      puts "Redirecting to #{meta_doc['default_topic']}"
       #if a deliverable was specified, grab the default topic from the .meta documents
       if(not(deliverable.nil?))
         begin
